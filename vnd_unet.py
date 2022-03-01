02c0dbbd4de6f080c45bb53fab2a4ece98151c5d
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-03-01 20:35:26 +0800

    default

--- vnd_unet.py
+++ vnd_unet.py
@@ -291,15 +291,17 @@ class VNDUnet(nn.Module):
         """
 
         criterion = nn.BCEWithLogitsLoss(size_average = False, reduce=False, reduction=None)
-        z_posterior = self.posterior_latent_space.rsample()
+        z_posterior_param = self.posterior_params
 
-        self.kl = self.kl_divergence(analytic=analytic_kl, calculate_posterior=False, z_posterior=z_posterior)
 
-        self.kl = torch.mean(self.kl)
 
         #Here we use the posterior sample sampled above
         self.reconstruction = self.reconstruct(use_posterior_mean=reconstruct_posterior_mean, calculate_posterior=False, z_posterior=z_posterior)
-        
+
+        # add this later
+        # self.kl = self.kl_divergence(analytic=analytic_kl, calculate_posterior=False, z_posterior=z_posterior)
+        # self.kl = torch.mean(self.kl)
+
         reconstruction_loss = criterion(input=self.reconstruction, target=segm)
         self.reconstruction_loss = torch.sum(reconstruction_loss)
         self.mean_reconstruction_loss = torch.mean(reconstruction_loss)
