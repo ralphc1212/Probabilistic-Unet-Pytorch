6e192e426d1ae8bed06641c9112c6a912eeedca0
ralphc <ralphc@ralphcdeMacBook-Air.local>
2022-03-02 13:55:02 +0800

    default

--- generate_indices.py
+++ generate_indices.py
@@ -1,4 +1,8 @@
 from load_LIDC_data import LIDC_IDRI
 dataset = LIDC_IDRI(dataset_location = '/home/nandcui/data/plidc-punet/')
+dataset_size = len(dataset)
+indices = list(range(dataset_size))
 
-print(dataset)
+
+print(dataset_size)
+print(indices)
