665d9d081ab5a8d99f53bcad0555d410ab43474c
ralphc <ralphc@ralphcdeMacBook-Air.local>
2022-03-02 14:42:31 +0800

    default

--- generate_indices.py
+++ generate_indices.py
@@ -3,6 +3,21 @@ dataset = LIDC_IDRI(dataset_location = '/home/nandcui/data/plidc-punet/')
 dataset_size = len(dataset)
 indices = list(range(dataset_size))
 
+# generate 10-fold cross validation set
+import numpy as np
+import csv
 
-print(dataset_size)
-print(indices)
+nsplits = 10
+
+split = int(np.floor(0.1 * dataset_size))
+
+for i in range(nsplits):
+	np.random.shuffle(indices)
+	train = indices[:-int(2*split)]
+	val = indices[-int(2*split):-int(split)]
+	test = indices[-int(split):]
+
+	split_list = [train, val, test]
+	with open('splits/split_{}.csv'.format(i), 'w') as f:
+		writer = csv.writer(f)
+		writer.writerows(split_list)
