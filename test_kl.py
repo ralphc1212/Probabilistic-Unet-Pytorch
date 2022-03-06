dabd813caddba683147718f0497b09b4357be862
ralphc <ralphc@ralphcdeMacBook-Air.local>
2022-03-06 19:58:51 +0800

    default

--- test_kl.py
+++ test_kl.py
@@ -11,7 +11,7 @@ log_var_pos = torch.tensor([- 0.5])
 std_pr = torch.exp(0.5 * log_var_pr)
 std_pos = torch.exp(0.5 * log_var_pos)
 
-kld = log_var_pr - log_var_pos + (torch.exp(log_var_pos) + torch.square(mu_pos - mu_pr)) / (2 * torch.exp(log_var_pos)) - 0.5
+kld = log_var_pr - log_var_pos + (torch.exp(log_var_pos) + torch.square(mu_pos - mu_pr)) / (2 * torch.exp(log_var_pr)) - 0.5
 
 dist_pr = Normal(loc=mu_pr, scale=std_pr)
 dist_pos = Normal(loc=mu_pos, scale=std_pos)
