533059f3d85151e43b0582e53048451a2a0c692b
ralphc <ralphc@ralphcdeMacBook-Air.local>
2022-03-10 22:13:01 +0800

    default

--- test_model.py
+++ test_model.py
@@ -56,7 +56,7 @@ def test(fold=0, dataloader=None, savefig=False):
 
         recons = torch.cat(recons)
 
-        print(torch.sigmoid(recons)[:4,0,32:80,32:80])
+        print(torch.sigmoid(recons)[:4,0,58:72,58:72])
         exit()
 
         torchvision.utils.save_image(patch, 
